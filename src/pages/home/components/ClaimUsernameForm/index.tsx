3655dba2f53e33c2c531681d88270beec160665e
MiguelMontenegroo <miguellaticiniosmontenegro@gmail.com>
2024-08-26 17:08:13 -0300

    developing further into the project

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -8,46 +8,57 @@ import { useRouter } from 'next/router'
 
 const claimUsernameFormSchema = z.object({
   username: z
-  .string()
-  .min(3, { message: 'O usuário precisa ter pelo menos 3 letras.' })
-  .regex(/^([a-z\\-]+)$/i, { message: 'O usuário pode ter apenas letras e hifens.' })
-  .transform(username => username.toLowerCase()),
+    .string()
+    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras.' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O usuário pode ter apenas letras e hifens.',
+    })
+    .transform((username) => username.toLowerCase()),
 })
 
 type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
-  const { 
+  const {
     register,
     handleSubmit,
     formState: { errors, isSubmitting },
   } = useForm<ClaimUsernameFormData>({
     resolver: zodResolver(claimUsernameFormSchema),
   })
- 
+
   const router = useRouter()
 
   async function handleClaimUsername(data: ClaimUsernameFormData) {
-  const { username } = data
+    const { username } = data
 
-   await router.push(`/register?username=${username}`)
+    await router.push(`/register?username=${username}`)
   }
 
   return (
     <>
-    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
-      <TextInput size="sm" prefix="ignite.com/" placeholder="seu-usuario" {...register('username')} crossOrigin={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} />
-      <Button size="sm" type="submit" disabled={isSubmitting}>
-        Reservar
-        <ArrowRight />
-      </Button >
-   
-    </Form>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          size="sm"
+          prefix="ignite.com/"
+          placeholder="seu-usuario"
+          {...register('username')}
+          crossOrigin={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        />
+        <Button size="sm" type="submit" disabled={isSubmitting}>
+          Reservar
+          <ArrowRight />
+        </Button>
+      </Form>
       <FormAnnotation>
-      <Text size="sm">
-        {errors.username ? errors.username.message : 'Digite o nome do usuário desejado'}
-      </Text>
-     </FormAnnotation>
-     </>
+        <Text size="sm">
+          {errors.username
+            ? errors.username.message
+            : 'Digite o nome do usuário desejado'}
+        </Text>
+      </FormAnnotation>
+    </>
   )
 }
